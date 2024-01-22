fc11b7f28bd3459231da25fc80077e3cb31ea637
이민우 <lmw7414@naver.com>
2024-01-22 11:23:53 +0900

    backend initial project setting

---
+++ backend/ByeolDam/src/main/java/com/ssafy/star/ByeolDamApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByeolDamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByeolDamApplication.class, args);
+    }
+
+}
