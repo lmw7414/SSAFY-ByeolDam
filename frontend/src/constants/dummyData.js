5528a155b9b83b10d27ffac2acc8cb418d1b6901
황정민 <sini3181@naver.com>
2024-01-26 21:52:45 +0900

    git amend를 첫번째
    Change-Id: I55886ba325484885a326c2c77cc60f125326aee1

--- frontend/src/constants/dummyData.js
+++ frontend/src/constants/dummyData.js
@@ -1,12 +1,12 @@
 const constellationList = [
-  { id: 0, url: './src/assets/images/cat.png', position: [0, 4, 10] },
-  { id: 1, url: './src/assets/images/cat.png', position: [0, 4, -10] },
-  { id: 2, url: './src/assets/images/cat.png', position: [-10, 4, 0] },
-  { id: 3, url: './src/assets/images/cat.png', position: [-7, 3, 7] },
-  { id: 4, url: './src/assets/images/cat.png', position: [7, 3, 7] },
-  { id: 5, url: './src/assets/images/cat.png', position: [-7, 3, -7] },
-  { id: 6, url: './src/assets/images/cat.png', position: [7, 3, -7] },
-  { id: 7, url: './src/assets/images/cat.png', position: [10, 4, 0] },
+  { id: 0, url: './src/assets/images/cat.png', position: [0, 6, 20] },
+  { id: 1, url: './src/assets/images/cat.png', position: [0, 6, -20] },
+  { id: 2, url: './src/assets/images/cat.png', position: [-20, 6, 0] },
+  { id: 3, url: './src/assets/images/cat.png', position: [-16, 5, 16] },
+  { id: 4, url: './src/assets/images/cat.png', position: [16, 5, 16] },
+  { id: 5, url: './src/assets/images/cat.png', position: [-16, 5, -16] },
+  { id: 6, url: './src/assets/images/cat.png', position: [16, 5, -16] },
+  { id: 7, url: './src/assets/images/cat.png', position: [20, 6, 0] },
 ];
 
 export default constellationList;
