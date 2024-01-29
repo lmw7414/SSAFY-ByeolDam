9c67b9661bf960b16a10c7c803c0932670748532
이창헌 <changbill@naver.com>
2024-01-29 11:07:19 +0900

    #2 - feat: article repository 커밋
    article repository 커밋

---
+++ backend/src/main/java/com/ssafy/star/article/dao/ArticleRepository.java
@@ -0,0 +1,21 @@
+package com.ssafy.star.article.dao;
+
+import com.ssafy.star.article.domain.ArticleEntity;
+import com.ssafy.star.article.domain.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
+        Page<ArticleEntity> findAllByUser(UserEntity entity, Pageable pageable);
+
+        @Modifying
+        @Query("update ArticleEntity a set a.hits = a.hits + 1 where a.id = :id")
+        long updateHits(@Param("id") Long id);
+}
+
