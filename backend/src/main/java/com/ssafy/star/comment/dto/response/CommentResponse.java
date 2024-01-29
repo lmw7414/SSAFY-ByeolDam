01f83c18f0add4821686262b041de3450aa084d0
차현철 <ckguscjf03@gmail.com>
2024-01-29 10:39:58 +0900

    #8 - feat : CommentResponse 생성
    Comment Response를 생성하였다.
    
    Change-Id: I02eb9b30fe106854c4b5808ef96b57bcb3e5693f

---
+++ backend/src/main/java/com/ssafy/star/comment/dto/response/CommentResponse.java
@@ -0,0 +1,31 @@
+package com.ssafy.star.comment.dto.response;
+
+import com.ssafy.star.comment.domain.CommentEntity;
+import com.ssafy.star.comment.dto.CommentDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class CommentResponse {
+    private Long id;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    private Long parentCommentId;
+    private Set<CommentEntity> childrenComments;
+
+    public static CommentResponse fromComment(CommentDto commentDto) {
+        return new CommentResponse(
+                commentDto.id(),
+                commentDto.content(),
+                commentDto.createdAt(),
+                commentDto.modifiedAt(),
+                commentDto.parentCommentId(),
+                commentDto.childrenComments()
+        );
+    }
+}
