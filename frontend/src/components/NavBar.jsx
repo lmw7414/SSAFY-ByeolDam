1e90f79828767c7bc2b3cc3f479a4aac6bfb28da
이은규 <eung9702@gmail.com>
2024-01-29 15:46:54 +0900

    #23 - feat: 네비게이션 바 기본틀 제작 완료
    토글버튼 제작, 토글 버튼 이미지 삽입
    
    Change-Id: I9c223ddd42779613a5f899eb048d486bc5a6f717

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,96 +1,123 @@
 export default function NavBar() {
   return (
-    <div className="nav-bar-container">
-      <div className="nav-bar-logo">
-        <img src="/src/assets/images/temporary-logo.png" alt="logo" />
-      </div>
-      <div className="nav-bar-profile-box">
-        <div className="profile-image-box">
-          <div className="profile-image-container">
-            <img className="profile-image" src="/src/assets/images/space.png" alt="profile_image" />
-          </div>
+    <div className="nav-bar-big-container">
+      <div className="nav-bar-container">
+        <div className="nav-bar-logo">
+          <img src="/src/assets/images/temporary-logo.png" alt="logo" />
         </div>
-        <div className="nickname">
-          <p>상당히느긋한사람</p>
-        </div>
-        <div className="profile-info-box">
-          <div className="profile-info">
-            <div class="profile-info-text button-light">
-              <p>별</p>
-            </div>
-            <div class="profile-info-num button-bold">
-              <p>24</p>
-            </div>
-          </div>
-          <div className="profile-info">
-            <div class="profile-info-text button-light">
-              <p>별자리</p>
-            </div>
-            <div class="profile-info-num button-bold">
-              <p>24</p>
-            </div>
-          </div>
-          <div className="profile-info">
-            <div class="profile-info-text button-light">
-              <p>팔로워</p>
-            </div>
-            <div class="profile-info-num button-bold">
-              <p>24</p>
-            </div>
-          </div>
-          <div className="profile-info">
-            <div class="profile-info-text button-light">
-              <p>팔로잉</p>
-            </div>
-            <div class="profile-info-num button-bold">
-              <p>24</p>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <hr />
+        <div className="nav-bar-profile-box">
+          <div className="profile-image-box">
+            <div className="profile-image-container">
+              <img
+                className="profile-image"
+                src="/src/assets/images/space.png"
+                alt="profile_image"
+              />
+            </div>
+          </div>
+          <div className="nickname title">
+            <p>상당히느긋한사람</p>
+          </div>
+          <div className="profile-info-box">
+            <div className="profile-info">
+              <div class="profile-info-text button-light">
+                <p>별</p>
+              </div>
+              <div class="profile-info-num button-bold">
+                <p>24</p>
+              </div>
+            </div>
+            <div className="profile-info">
+              <div class="profile-info-text button-light">
+                <p>별자리</p>
+              </div>
+              <div class="profile-info-num button-bold">
+                <p>24</p>
+              </div>
+            </div>
+            <div className="profile-info">
+              <div class="profile-info-text button-light">
+                <p>팔로워</p>
+              </div>
+              <div class="profile-info-num button-bold">
+                <p>24</p>
+              </div>
+            </div>
+            <div className="profile-info">
+              <div class="profile-info-text button-light">
+                <p>팔로잉</p>
+              </div>
+              <div class="profile-info-num button-bold">
+                <p>24</p>
+              </div>
+            </div>
+          </div>
+        </div>
 
-      <div className="nav-bar-menu-box">
-        <div className="nav-bar-menu">
-          <img
-            className="nav-bar-menu-icon"
-            src="/src/assets/images/nav-bar-menu-icons/search.png"
-            alt="search_icon"
-          />
-          <p className="nav-bar-menu-text button-light">검색</p>
+        <hr />
+
+        <div className="nav-bar-menu-box">
+          <div className="nav-bar-menu">
+            <div className="nav-bar-menu-icon-box">
+              <img
+                className="nav-bar-menu-icon"
+                src="/src/assets/images/nav-bar-menu-icons/search.png"
+                alt="search_icon"
+              />
+            </div>
+            <p className="nav-bar-menu-text button-light">검색</p>
+          </div>
+          <div className="nav-bar-menu">
+            <div className="nav-bar-menu-icon-box">
+              <img
+                className="nav-bar-menu-icon"
+                src="/src/assets/images/nav-bar-menu-icons/feed.png"
+                alt="feed_icon"
+              />
+            </div>
+            <p className="nav-bar-menu-text button-light">피드</p>
+          </div>
+          <div className="nav-bar-menu">
+            <div className="nav-bar-menu-icon-box">
+              <img
+                className="nav-bar-menu-icon"
+                src="/src/assets/images/nav-bar-menu-icons/notification.png"
+                alt="notification_icon"
+              />
+            </div>
+            <p className="nav-bar-menu-text button-light">알림</p>
+          </div>
+          <div className="nav-bar-menu">
+            <div className="nav-bar-menu-icon-box">
+              <img
+                className="nav-bar-menu-icon"
+                src="/src/assets/images/nav-bar-menu-icons/settings.png"
+                alt="settings_icon"
+              />
+            </div>
+            <p className="nav-bar-menu-text button-light">설정</p>
+          </div>
+          <div className="nav-bar-menu">
+            <div className="nav-bar-menu-icon-box">
+              <img
+                className="nav-bar-menu-icon"
+                src="/src/assets/images/nav-bar-menu-icons/logout.png"
+                alt="logout_icon"
+              />
+            </div>
+            <p className="nav-bar-menu-text button-light">로그아웃</p>
+          </div>
         </div>
-        <div className="nav-bar-menu">
-          <img
-            className="nav-bar-menu-icon"
-            src="/src/assets/images/nav-bar-menu-icons/feed.png"
-            alt="feed_icon"
-          />
-          <p className="nav-bar-menu-text button-light">피드</p>
-        </div>
-        <div className="nav-bar-menu">
-          <img
-            className="nav-bar-menu-icon"
-            src="/src/assets/images/nav-bar-menu-icons/notification.png"
-            alt="notification_icon"
-          />
-          <p className="nav-bar-menu-text button-light">알림</p>
-        </div>
-        <div className="nav-bar-menu">
-          <img
-            className="nav-bar-menu-icon"
-            src="/src/assets/images/nav-bar-menu-icons/settings.png"
-            alt="settings_icon"
-          />
-          <p className="nav-bar-menu-text button-light">설정</p>
-        </div>
-        <div className="nav-bar-menu">
-          <img
-            className="nav-bar-menu-icon"
-            src="/src/assets/images/nav-bar-menu-icons/logout.png"
-            alt="logout_icon"
-          />
-          <p className="nav-bar-menu-text button-light">로그아웃</p>
+
+        <div className="nav-bar-toggle-container">
+          <div className="nav-bar-menu-icon-box">
+            <img
+              src="/src/assets/images/nav-bar-toggle-button/close.png"
+              alt="toggle_button"
+              className="toggle-button"
+            />
+          </div>
         </div>
       </div>
     </div>
