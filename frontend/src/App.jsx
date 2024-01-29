8d60edcefd23bc7400d9ee6f90441714d8f82c24
이은규 <eung9702@gmail.com>
2024-01-29 12:40:46 +0900

    #17 - feat : CSS 디렉토리 구조 변경 style에 component 폴더 생성
    Change-Id: I43b46a7de0ccc06dbe5e66630d89dec4824aa3b4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Route } from 'wouter';
 
 import Member from './pages/member/Member';
 import Universe from './pages/Universe';
+import NavBar from './components/NavBar';
 
 import './assets/styles/scss/main.scss';
 
 export default function App() {
   return (
     <div className="App">
+      <NavBar />
       <Route path="/" component={Member} />
       <Route path="/home" component={Universe} />
     </div>
