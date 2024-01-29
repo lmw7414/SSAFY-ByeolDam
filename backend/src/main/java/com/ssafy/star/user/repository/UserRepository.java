f5980c76d26de6fac1bd7e237ea71f401c330095
이민우 <lmw7414@naver.com>
2024-01-29 10:18:48 +0900

    18 - feat : 유저 Entity 및 DTO 클래스 생성
    유저 Entity 클래스를 만들고 DB에 테이블이 정상적으로 생성되는 것까지 확인하였다.

---
+++ backend/src/main/java/com/ssafy/star/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.star.user.repository;
+
+import com.ssafy.star.user.domain.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByNickname(String nickname);
+}
