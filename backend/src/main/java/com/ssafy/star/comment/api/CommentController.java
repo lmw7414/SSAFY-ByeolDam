a4a244017109df6881a0c7164730c5e91eed281f
차현철 <ckguscjf03@gmail.com>
2024-01-31 15:19:23 +0900

    #8 - refactor : CommentController Request Mapping 수정
    명확한  형식으로 변경
    
    Change-Id: I69d8fbf39e1ed3edd635c4774752e31283c33394

--- backend/src/main/java/com/ssafy/star/comment/api/CommentController.java
+++ backend/src/main/java/com/ssafy/star/comment/api/CommentController.java
@@ -1,10 +1,11 @@
 package com.ssafy.star.comment.api;
 
 import com.ssafy.star.comment.application.CommentService;
+import com.ssafy.star.comment.dto.CommentDto;
 import com.ssafy.star.comment.dto.request.CommentCreateRequest;
 import com.ssafy.star.comment.dto.request.CommentModifyRequest;
 import com.ssafy.star.comment.dto.response.CommentResponse;
-import com.ssafy.star.comment.dto.response.Response;
+import com.ssafy.star.common.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,35 +14,33 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/api/v1/comments")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class CommentController {
 
     private final CommentService commentService;
 
     // 댓글 조회
-    @GetMapping("/articles/{articleId}")
+    @GetMapping("/articles/{articleId}/comments")
     public Response<Page<CommentResponse>> search(@PathVariable Long articleId, Pageable pageable) {
         return Response.success(commentService.search(articleId, pageable).map(CommentResponse::fromComment));
     }
 
     // 댓글 생성
     // commentId가 들어오는 경우 - 대댓글, null인 경우 - 댓글
-    @PostMapping("/articles/{articleId}")
+    @PostMapping("/articles/{articleId}/comments")
     public Response<Void> create(@RequestBody CommentCreateRequest request, Authentication authentication) {
         commentService.create(request.getArticleId(), authentication.getName(), request.getContent(), request.getCommentId());
-        return Response.success();
     }
 
     // 댓글 수정
-    @PutMapping("/{commentId}")
+    @PutMapping("/comments/{commentId}")
     public Response<Void> modify(@RequestBody CommentModifyRequest request, Authentication authentication) {
         commentService.modify(request.getCommentId(), authentication.getName(), request.getContent());
-        return Response.success();
     }
 
     // 댓글 삭제
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping("/comments/{commentId}")
     public Response<Void> delete(@PathVariable Long commentId, Authentication authentication) {
         commentService.delete(commentId, authentication.getName());
         return Response.success();
