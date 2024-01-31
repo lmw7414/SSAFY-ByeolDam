f82a8b233db2058201e479f6e7e4c7dd06f9009d
황정민 <sini3181@naver.com>
2024-01-31 17:06:09 +0900

    #30 - fix : modal test 에러 해결
    useEffect로 한번만 실행하게 만드는것으로 무한 루프 해결
    
    Change-Id: I1a10135a7108f2299b03d7a5ba5ad4b12c72f304

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar';
 import './assets/styles/scss/main.scss';
 import ModalContainer from './components/modal/ModalContainer';
 import { ModalContext } from './hooks/useModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ConstellationListModal from './components/modal/ConstellationListModal';
 
 export default function App() {
@@ -18,11 +18,14 @@ export default function App() {
     title: '',
   });
 
-  setModalState({
-    isOpen: true,
-    title: '별자리 리스트',
-    children: <ConstellationListModal />,
-  });
+  // 한번만 실행
+  useEffect(() => {
+    setModalState({
+      isOpen: true,
+      title: '별자리 리스트',
+      children: <ConstellationListModal />,
+    });
+  }, []);
 
   return (
     <div className="App">
