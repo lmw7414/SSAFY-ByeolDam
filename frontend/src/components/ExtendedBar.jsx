f87ac3c2b14ae520c1fc05260bbe7819f7b5fe82
이은규 <eung9702@gmail.com>
2024-01-31 10:28:36 +0900

    #27 - feat : 네비게이션바, 확장바 컴포넌트화
    ProfileInfo, NavBarMenu, ExtendedBarIcon 컴포넌트 제작하여 네비게이션바 및 확장바 하위 컴포넌트 모듈화
    네비게이션바, 확장바 css 수정
    
    Change-Id: I7bae80391f0d65db531d8e67578b9e513b1b6257

--- frontend/src/components/ExtendedBar.jsx
+++ frontend/src/components/ExtendedBar.jsx
@@ -1,9 +1,9 @@
 // import { useState } from 'react';
+import ExtendedBarIcon from './base/ExtendedBarIcon';
 export default function ExtendedBar({ changeBar, barId }) {
   const ToggleClick = () => {
     changeBar(0);
   };
-
   const openSearchBar = () => {
     changeBar(1);
   };
@@ -18,62 +18,81 @@ export default function ExtendedBar({ changeBar, barId }) {
           <img src="/src/assets/images/temporary-logo.png" alt="logo" />
         </div>
 
-        {barId === 1 ? <input type="text" placeholder="검색" className="search-bar-input" /> : null}
+        <div className="extended-bar-contents">
+          {barId === 1 ? (
+            <input type="text" placeholder="검색" className="search-bar-input" />
+          ) : null}
 
-        <hr className="extended-bar-divide-line" />
+          <hr className="extended-bar-divide-line" />
 
-        <div className="extended-bar-menu-box">
-          <div className="extended-bar-menu">
-            <div className="extended-bar-menu-icon-box">
-              <img
-                className="extended-bar-menu-icon"
-                src="/src/assets/images/nav-bar-menu-icons/search.png"
-                alt="search_icon"
-                onClick={openSearchBar}
-              />
+          <div className="search-bar-bottom-contents">
+            <div className="extended-bar-side-menu-box">
+              <div
+                className={
+                  barId === 1 ? 'search-bar-filter-box' : 'search-bar-filter-box opacity-0'
+                }
+              >
+                <ExtendedBarIcon
+                  src={'/src/assets/images/search-bar-filter-icons/user.png'}
+                  alt={'user'}
+                />
+                <ExtendedBarIcon
+                  src={'/src/assets/images/search-bar-filter-icons/star.png'}
+                  alt={'star'}
+                />
+                <ExtendedBarIcon
+                  src={'/src/assets/images/search-bar-filter-icons/constellation.png'}
+                  alt={'constellation'}
+                />
+              </div>
+
+              <div className="extended-bar-menu-box">
+                <ExtendedBarIcon
+                  src={
+                    barId === 1
+                      ? '/src/assets/images/nav-bar-menu-icons/search_activated.png'
+                      : '/src/assets/images/nav-bar-menu-icons/search.png'
+                  }
+                  alt={'search'}
+                  onClick={openSearchBar}
+                />
+                <ExtendedBarIcon
+                  src={'/src/assets/images/nav-bar-menu-icons/feed.png'}
+                  alt={'feed'}
+                  className={'extended-bar-menu-icon'}
+                />
+                <ExtendedBarIcon
+                  src={
+                    barId === 2
+                      ? '/src/assets/images/nav-bar-menu-icons/notifications_activated.png'
+                      : '/src/assets/images/nav-bar-menu-icons/notifications.png'
+                  }
+                  alt={'notifications'}
+                  onClick={openNotificationBar}
+                />
+                <ExtendedBarIcon
+                  src={'/src/assets/images/nav-bar-menu-icons/settings.png'}
+                  alt={'notifications'}
+                />
+                <ExtendedBarIcon
+                  src={'/src/assets/images/nav-bar-menu-icons/logout.png'}
+                  alt={'logout'}
+                />
+              </div>
             </div>
-          </div>
-          <div className="extended-bar-menu">
-            <div className="extended-bar-menu-icon-box">
-              <img
-                className="extended-bar-menu-icon"
-                src="/src/assets/images/nav-bar-menu-icons/feed.png"
-                alt="feed_icon"
-              />
+
+            <div>
+              <hr className="extended-bar-column-divide-line" />
             </div>
-          </div>
-          <div className="extended-bar-menu">
-            <div className="extended-bar-menu-icon-box">
-              <img
-                className="extended-bar-menu-icon"
-                src="/src/assets/images/nav-bar-menu-icons/notification.png"
-                alt="notification_icon"
-                onClick={openNotificationBar}
-              />
-            </div>
-          </div>
-          <div className="extended-bar-menu">
-            <div className="extended-bar-menu-icon-box">
-              <img
-                className="extended-bar-menu-icon"
-                src="/src/assets/images/nav-bar-menu-icons/settings.png"
-                alt="settings_icon"
-              />
-            </div>
-          </div>
-          <div className="extended-bar-menu">
-            <div className="extended-bar-menu-icon-box">
-              <img
-                className="extended-bar-menu-icon"
-                src="/src/assets/images/nav-bar-menu-icons/logout.png"
-                alt="logout_icon"
-              />
+
+            <div className="extended-bar-main-contents-box">
+              <p className="title nickname">texts</p>
             </div>
           </div>
         </div>
 
-        <div className="extended-bar-toggle-container" onClick={ToggleClick}>
-          <div className="extended-bar-menu-icon-box">
+        <div className="nav-bar-toggle-container" onClick={ToggleClick}>
+          <div className="nav-bar-menu-icon-box">
             <img
               src="/src/assets/images/nav-bar-toggle-button/close.png"
               alt="toggle_button"
