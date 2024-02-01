4c5894b2fcc3433bf96a1f70012f7e6385396ae4
이민우 <lmw7414@naver.com>
2024-02-01 13:01:10 +0900

    #28 - feat : FollowEntity 설계
    팔로우 팔로잉을 위한 엔티티를 설계하였다.
    
    Change-Id: I51ce3bcedb45525f4258c8c8631b4876a8b39eca

---
+++ backend/src/main/java/com/ssafy/star/user/domain/FollowEntity.java
@@ -0,0 +1,53 @@
+package com.ssafy.star.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@ToString
+@Table(name = "follow")
+public class FollowEntity {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "from_user_id")
+    private UserEntity fromUser;
+
+    @ManyToOne
+    @JoinColumn(name = "to_user_id")
+    private UserEntity toUser;
+
+    private LocalDateTime requestDate;
+
+    private LocalDateTime acceptDate;
+
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private ApprovalStatus status;
+
+    @PrePersist
+    void requestDate() {
+        this.requestDate = LocalDateTime.from(LocalDateTime.now());
+    }
+
+    protected FollowEntity(){}
+
+    private FollowEntity(UserEntity fromUser, UserEntity toUser, LocalDateTime acceptDate, ApprovalStatus status) {
+        this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.acceptDate = acceptDate;
+        this.status = status;
+    }
+    public static FollowEntity of(UserEntity fromUser, UserEntity toUser, LocalDateTime acceptDate, ApprovalStatus status) {
+        return new FollowEntity(fromUser, toUser, acceptDate, status);
+    }
+
+}
