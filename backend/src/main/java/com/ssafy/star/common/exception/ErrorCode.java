4b1956085b1e667f27a359929ed8045c60602dab
이민우 <lmw7414@naver.com>
2024-02-01 13:24:27 +0900

    #28 - feat : 팔로워 관련 컨트롤러 기능 설계
    TODO: 사용자가 삭제될 때 팔로워 / 팔로잉 정보도 사라지도록 구현해야함
    Change-Id: Ic124288d3c7e589cf89bccf4754ff22587d30248

--- backend/src/main/java/com/ssafy/star/common/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/star/common/exception/ErrorCode.java
@@ -10,10 +10,13 @@ public enum ErrorCode {
     DUPLICATED_USER_EMAIL(HttpStatus.CONFLICT, "User email is duplicated"),
     DUPLICATED_USER_NICKNAME(HttpStatus.CONFLICT, "User nickname is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    SELF_FOLLOW_ERROR(HttpStatus.BAD_REQUEST, "Self following is invalid"),
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "Article not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "Token is invalid"),
     INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "Permission is invalid"),
+    FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "Follow status not founded in follow list"),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Request is invalid"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server error");
 
     private HttpStatus status;
