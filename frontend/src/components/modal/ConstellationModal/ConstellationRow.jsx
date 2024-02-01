be222254a7aceb3b122d60c0e14ba7889d3d3e96
이은규 <eung9702@gmail.com>
2024-02-01 15:05:48 +0900

    #33 - feat : 별자리 리스트 모달 컴포넌트 제작중
    별자리 리스트 모달 컴포넌트 제작
    별자리 행 컴포넌트 제작
    별자리 생성 및 게시글 생성 버튼 제작
    
    Change-Id: Iba162841ea58a5daf7e9e0422ef612210eaed527

---
+++ frontend/src/components/modal/ConstellationModal/ConstellationRow.jsx
@@ -0,0 +1,8 @@
+export default function ConstellationRow() {
+  return (
+    <div className="constellation-row">
+      <div className="constellation-thumbnail-container"></div>
+      <div className="star-list-container">사진들</div>
+    </div>
+  );
+}
