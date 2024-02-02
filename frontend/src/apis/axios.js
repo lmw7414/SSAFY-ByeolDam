ef4d749dda7f2f2f793ab13b83c8f7eae3589d3a
황정민 <sini3181@naver.com>
2024-02-02 13:57:51 +0900

    #38 - feat : proxy 설정
    CORS 문제를 해결하기 위해 vite.config.js에 proxy 설정
    
    Change-Id: Iadd0c7ef6a82b008620e5f042603946c8b2c6fef

--- frontend/src/apis/axios.js
+++ frontend/src/apis/axios.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: import.meta.env.VITE_BASE_URL });
+const client = axios.create({
+  baseURL: '/api/v1/',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
 export default client;
