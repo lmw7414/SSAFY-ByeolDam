6c76514d77c4b889e9c2deb22001e012046935a7
이민우 <lmw7414@naver.com>
2024-02-02 14:38:39 +0900

    cors 설정
    Change-Id: I23b925dc68cb7f2afe55792c593d1800e8e54425

--- backend/src/main/java/com/ssafy/star/global/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/star/global/auth/config/SecurityConfig.java
@@ -15,12 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -41,36 +37,31 @@ public class SecurityConfig {
             "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html"
     };
 
-
     @Bean
-    public CorsConfigurationSource configurationSource() {
-        return request -> {
-            CorsConfiguration corsConfiguration = new CorsConfiguration();
-            corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://i10b309.p.ssafy.io:5173"));
-            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-            corsConfiguration.setAllowCredentials(true);
-            corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Authorization-refresh", "Cache-Control", "Content-Type"));
-
-            /* 응답 헤더 설정 추가*/
-            corsConfiguration.setExposedHeaders(Arrays.asList("Authorization", "Authorization-refresh"));
-            corsConfiguration.setMaxAge(3600L); //preflight 결과를 1시간동안 캐시에 저장
-            return corsConfiguration;
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(corsConfigurer -> corsConfigurer.configurationSource(configurationSource()))
-                .csrf(AbstractHttpConfigurer::disable);
-        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize.requestMatchers("/api/*/users/join", "/api/*/users/login", "/api/*/users/check-email", "/api/*/users/check-nickname").permitAll()
                                 .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll().requestMatchers(AUTH_WHITELIST).permitAll()
                                 .requestMatchers("/api/**").authenticated()
                 ).sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                ).addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+                )
+                .addFilter(corsFilter())
+                .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionManager ->
                         exceptionManager.authenticationEntryPoint(new CustomAuthenticationEntryPoint())
                 );
