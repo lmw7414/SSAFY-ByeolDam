3fe7ddc2df4cb79f1d78917135cd481c831e53fd
이민우 <lmw7414@naver.com>
2024-02-03 23:21:35 +0900

    #36 - feat : 이메일 관련 에러코드 추가
    이메일 검증에 필요한 에러 코드 및 메시지를 정의하였다.
    
    Change-Id: Ic7a18279d253af56c4c2cc8b871ad09d29cc3653

--- backend/src/main/java/com/ssafy/star/common/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/star/common/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     DUPLICATED_USER_EMAIL(HttpStatus.CONFLICT, "User email is duplicated"),
     DUPLICATED_USER_NICKNAME(HttpStatus.CONFLICT, "User nickname is duplicated"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not founded"),
+    UNSUITABLE_NICKNAME(HttpStatus.BAD_REQUEST, "Nickname does not meet the condition"),
+    UNSUITABLE_EMAIL(HttpStatus.BAD_REQUEST, "Email does not meet the condition"),
     SELF_FOLLOW_ERROR(HttpStatus.BAD_REQUEST, "Self following is invalid"),
     ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "Article not founded"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
@@ -19,6 +21,9 @@ public enum ErrorCode {
     INVALID_CONTENT(HttpStatus.BAD_REQUEST, "Content type is invalid"),
     FOLLOW_NOT_FOUND(HttpStatus.NOT_FOUND, "Follow status not founded in follow list"),
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "Request is invalid"),
+    UNABLE_TO_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "Email failed to send"),
+    NEVER_ATTEMPT_EMAIL_AUTH(HttpStatus.BAD_REQUEST, "이메일 인증을 요청한 적이 없음"),
+    INVALID_EMAIL_CODE(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않음"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server error");
 
 
