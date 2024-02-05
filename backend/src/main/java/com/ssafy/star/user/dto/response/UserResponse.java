98a57221f21c90b899d8ec2337606adc0bd2f682
이민우 <lmw7414@naver.com>
2024-02-05 14:53:55 +0900

    #49 - feat : UserResponse 필드 추가
    프로필에 더 많은 정보를 보여주기 위해 필드를 추가함. 이후 범위에 따라 dto를 나눠 분리할 수도 있을 것 같다.
    
    Change-Id: Ie07ad9005e770bafa05ba1a683e8886afec21315

--- backend/src/main/java/com/ssafy/star/user/dto/response/UserResponse.java
+++ backend/src/main/java/com/ssafy/star/user/dto/response/UserResponse.java
@@ -1,16 +1,23 @@
 package com.ssafy.star.user.dto.response;
 
+import com.ssafy.star.common.types.DisclosureType;
 import com.ssafy.star.user.dto.User;
+
+import java.time.LocalDate;
 
 public record UserResponse(
         //TODO: 사진, 팔로워, 팔로잉 추가
+        String imageUrl,
+        String email,
         String name,
         String nickname,
-        String memo
+        String memo,
+        DisclosureType disclosureType,
+        LocalDate birthday
 
 ) {
 
     public static UserResponse fromUser(User dto) {
-        return new UserResponse(dto.name(), dto.nickname(), dto.memo());
+        return new UserResponse("imageUrl", dto.email(), dto.name(), dto.nickname(), dto.memo(), dto.disclosureType(), dto.birthday());
     }
 }
