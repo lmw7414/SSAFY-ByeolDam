fc2e1b7c59cd31b566918a7e0c69b6f0a0dd71ef
차현철 <ckguscjf03@gmail.com>
2024-02-05 17:24:13 +0900

    #50 - feat : s3 이미지 다운로드
    s3 url에 해당하는 이미지를 다운로드 받고 파일이름 반환
    
    Change-Id: I046c9e280fb269c06b77cb639e4266a15488931f

--- django/ByeolDam/ByeolDamAI/views.py
+++ django/ByeolDam/ByeolDamAI/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+import os
+import requests
+from django.http import JsonResponse
+from rest_framework.response import Response
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+@require_POST
+@csrf_exempt
 def getImage(request):
-    return 
+    if request.method == 'POST':
+        url = request.POST.get('url').strip('{}')
+
+        # URL에서 이미지 다운로드
+        response = requests.get(url)
+        if response.status_code == 200:
+            # 이미지를 저장할 경로 설정 (예시: media 폴더 내에 저장)
+            base_path = 'media/'
+            filename = os.path.join(base_path, 'downloaded_image.jpg')
+
+            # 이미지 저장
+            f = open(filename, 'wb')
+            f.write(response.content)
+            return JsonResponse({'image_url': "downloaded_image.jpg"})
+        else:
+            return JsonResponse({'error': 'Failed to download image'}, status=500)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
