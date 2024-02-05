67253493f086c430b43063b74d2f9e1c0ad73189
이은규 <eung9702@gmail.com>
2024-02-05 16:04:17 +0900

    #50 - feat : 팔로잉/팔로워 모달 파일 생성
    FollowingFollwer.jsx UserBox.jsx 생성 후 기본 요소 생성
    
    Change-Id: I681dc857ea35f27e2758a04de90c307efe1aa7fe

---
+++ frontend/src/components/modal/Follow/FollowingFollower.jsx
@@ -0,0 +1,11 @@
+import UserBox from './UserBox';
+export default function FollowingFollower() {
+  return (
+    <div>
+      <input type="text" />
+      <div>
+        <UserBox />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/modal/Follow/UserBox.jsx
@@ -0,0 +1,14 @@
+export default function UserBox() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        <img src="" alt="" />
+        13
+        <img src="" alt="" />
+        34
+        <button></button>
+      </div>
+    </div>
+  );
+}
