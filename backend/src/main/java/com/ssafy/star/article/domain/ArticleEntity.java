d9a3bcf0111615596f49ea64ab503aacde205d95
차현철 <ckguscjf03@gmail.com>
2024-02-06 11:14:12 +0900

    #21 - fix : Article or User 삭제 시 좋아요 DB 삭제 구현
    Article 혹은 User 삭제 시 좋아요 DB에 연관 데이터 삭제되도록 수정
    
    Change-Id: Ie81f937332a00f6529d2a54af9f1555c7cc07b79

--- backend/src/main/java/com/ssafy/star/article/domain/ArticleEntity.java
+++ backend/src/main/java/com/ssafy/star/article/domain/ArticleEntity.java
@@ -1,19 +1,17 @@
 package com.ssafy.star.article.domain;
 
 import com.ssafy.star.article.DisclosureType;
+import com.ssafy.star.like.domain.ArticleLikeEntity;
 import com.ssafy.star.user.domain.UserEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ManyToAny;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "\"article\"")
@@ -44,6 +42,9 @@ public class ArticleEntity {
 
     @Column(name = "disclosure", nullable = false)
     private DisclosureType disclosure;
+
+    @OneToMany(mappedBy = "articleEntity", orphanRemoval = true)
+    private List<ArticleLikeEntity> articleLikes = new ArrayList<>();
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
