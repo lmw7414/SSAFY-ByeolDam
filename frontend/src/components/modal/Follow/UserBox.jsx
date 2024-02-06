22526f1c38bfcd16fb805e1ad74e6db557e942e3
이은규 <eung9702@gmail.com>
2024-02-06 23:49:26 +0900

    #51 - feat : 팔로워/팔로잉 모달 구현
    팔로잉, 팔로워 유저 정보 api 정보 불러오기 구현
    
    Change-Id: Ib2ff44c04b5029af318e41e9b5fbbebd11c86f97

--- frontend/src/components/modal/Follow/UserBox.jsx
+++ frontend/src/components/modal/Follow/UserBox.jsx
@@ -1,13 +1,16 @@
-export default function UserBox() {
+export default function UserBox({ followSubject, profileImg, nickname }) {
   return (
     <div>
-      <img src="" alt="" />
+      <div className="follow-modal-profile-img-container">
+        <img className="follow-modal-profile-image" src={profileImg} alt="" />
+      </div>
+      <p>{nickname}</p>
       <div>
         <img src="" alt="" />
 
         <img src="" alt="" />
 
-        <button></button>
+        <button>{followSubject === 'following' ? '팔로잉' : '삭제'}</button>
       </div>
     </div>
   );
