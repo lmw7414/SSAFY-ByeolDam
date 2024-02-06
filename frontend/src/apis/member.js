62b75c36e038c83b7d13d950d6a56f512397e7cd
황정민 <sini3181@naver.com>
2024-02-06 11:02:33 +0900

    #45 - 이메일, 비밀번호 로그인 구현, header에 token 추가
    로그인 확인 가능하도록 상태관리, 임시적으로 token은 sessionStorage에 저장
    
    Change-Id: I7d908dbec9d31b9ad7f09ba0c7af5288d73df806

--- frontend/src/apis/member.js
+++ frontend/src/apis/member.js
@@ -1,45 +1,48 @@
 import axios from './axios';
 
 export const login = async ({ email, password }) => {
-  try {
-    const result = await axios.post('/users/login', {
-      email,
-      password,
-    });
+  const { data } = await axios.post('/users/login', {
+    email,
+    password,
+  });
 
-    sessionStorage.setItem('token', result?.token);
+  sessionStorage.setItem('token', data?.result?.token);
 
-    axios.interceptors.request.use(
-      (config) => {
-        config.headers['Authorization'] = `Bearer ${result.token}`;
-        return config;
-      },
-      (e) => {
-        return Promise.reject(e);
-      },
-    );
-
-    console.log(result);
-
-    return result;
-  } catch (e) {
-    console.error(e);
-  }
+  axios.interceptors.request.use(
+    (config) => {
+      config.headers['Authorization'] = `Bearer ${data?.result?.token}`;
+      return config;
+    },
+    (e) => {
+      return Promise.reject(e);
+    },
+  );
 };
 
 export const join = async ({ email, password, name, nickname }) => {
-  try {
-    const result = await axios.post('/users/join', {
-      email,
-      password,
-      name,
-      nickname,
-    });
+  const result = await axios.post('/users/join', {
+    email,
+    password,
+    name,
+    nickname,
+  });
 
-    console.log(result);
+  sessionStorage.setItem('userInfo', result);
+  return result;
+};
 
-    sessionStorage.setItem('userInfo', result);
-  } catch (e) {
-    console.error(e);
-  }
+export const checkNickname = async (nickname) => {
+  const result = await axios.post('/users/check-nickname', { nickname });
+  return result;
+};
+
+export const checkEmail = async (email) => {
+  const result = await axios.post('/users/check-email', { email });
+  return result;
+};
+
+export const getMyFollwings = async () => {
+  if (!sessionStorage.token) return;
+  const result = await axios.get('/me/followings');
+  console.log(result);
 };
--- frontend/src/pages/member/LoginTest.jsx
+++ frontend/src/pages/member/LoginTest.jsx
@@ -1,41 +1,51 @@
 import axios from '../../apis/axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { login } from '../../apis/member';
 
 export default function LoginTest() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
-  const login = async (event) => {
+  const userLogin = (event) => {
     event.preventDefault();
-    try {
-      const data = await axios.post('/users/login', {
-        email,
-        password,
+    login({ email, password })
+      .then(() => {
+        setIsLogin(true);
+      })
+      .catch((e) => {
+        alert('아이디 또는 비밀번호가 일치하지 않거나 존재하지 않는 아이디입니다.');
       });
-      console.log(data);
-    } catch (e) {
-      console.error(e);
-    }
   };
 
+  useEffect(() => {
+    setIsLogin(!!sessionStorage.token);
+  }, []);
+
   return (
-    <div>
-      <h1 className="loginText">로그인</h1>
-      <form className="loginForm" onSubmit={login}>
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="아이디"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="비밀번호"
-        />
-        <button type="submit">로그인</button>
-      </form>
-    </div>
+    <>
+      {isLogin && <div>로그인된 상태입니다.</div>}
+      {!isLogin && (
+        <div>
+          <h1 className="loginText">로그인</h1>
+          <form className="loginForm" onSubmit={userLogin}>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="아이디"
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="비밀번호"
+            />
+            <button type="submit">로그인</button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
