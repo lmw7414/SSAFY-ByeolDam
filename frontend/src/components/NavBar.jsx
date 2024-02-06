2db26fecd671334a2349d0e1d2d118ff993c20ff
이은규 <eung9702@gmail.com>
2024-02-06 12:39:58 +0900

    #23 - refactor : 네비게이션 바 분기점 리팩터링
    barId와 별개로 네비게이션바와 확장바를 번갈아서 띄우는 navEx state 추가
    확장바 css 간소화
    
    Change-Id: I1308a9141aa6bb0fcb16846ec9cc996f0f72e867

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import axios from '/src/apis/axios';
 import useModal from '../hooks/useModal';
@@ -12,27 +12,40 @@ import FollowContainer from './modal/Follow/FollowContainer';
 export default function NavBar() {
   const [isNavBarVisible, setIsNavBarVisible] = useState(true);
   const [barId, setBarId] = useState(0);
+  const [navEx, setNavEx] = useState(0);
   const [following, setFollowing] = useState(0);
   const [follower, setFollower] = useState(0);
   const [modalState, setModalState] = useModal();
+
+  const navigate = useNavigate();
+  const goHome = () => {
+    navigate('/home');
+    setBarId(0);
+  };
 
   const closeNavBar = () => {
     setIsNavBarVisible(!isNavBarVisible);
   };
   const openSearchBar = () => {
     setBarId(1);
+    setNavEx(1);
   };
   const openNotificationBar = () => {
     setBarId(3);
+    setNavEx(1);
   };
   const changeBar = (newId) => {
     setBarId(newId);
   };
+  const changeExNav = (newId) => {
+    setNavEx(newId);
+  };
 
-  const openSettings = () => {
+  const openSettingsPage = () => {
     setBarId(4);
   };
   console.log('barId : ', barId);
+  console.log('navEx : ', navEx);
 
   const readFollowing = async () => {
     const data = await axios.get('immigrant_co/count-followings');
@@ -71,16 +84,20 @@ export default function NavBar() {
 
   return (
     <div>
-      {barId ? (
-        <ExtendedBar changeBar={changeBar} barId={barId} />
+      {navEx ? (
+        <ExtendedBar
+          changeBar={changeBar}
+          barId={barId}
+          changeExNav={changeExNav}
+          navEx={navEx}
+          goHome={goHome}
+        />
       ) : (
         <div className={`nav-bar-big-container ${isNavBarVisible ? '' : 'nav-bar-hidden'}`}>
           <div className="nav-bar-container">
-            <Link to={'/home'}>
-              <div className="nav-bar-logo">
-                <img src="/src/assets/images/temporary-logo.png" alt="logo" />
-              </div>
-            </Link>
+            <div className="nav-bar-logo" onClick={goHome}>
+              <img src="/src/assets/images/temporary-logo.png" alt="logo" />
+            </div>
 
             <div className="nav-bar-contents">
               <div className="nav-bar-profile-box">
@@ -132,9 +149,14 @@ export default function NavBar() {
                 <Link to={'/settings'} className="link-setting">
                   <NavBarMenu
                     text={'설정'}
-                    src={'/src/assets/images/nav-bar-menu-icons/settings.png'}
+                    src={
+                      barId === 4
+                        ? '/src/assets/images/nav-bar-menu-icons/settings_activated.png'
+                        : '/src/assets/images/nav-bar-menu-icons/settings.png'
+                    }
                     alt={'settings'}
-                    onClick={openSettings}
+                    onClick={openSettingsPage}
+                    barId={barId}
                   />
                 </Link>
                 <NavBarMenu
