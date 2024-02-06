26791b10fd36339934c87f4ca3a55f62b6800156
김수혁 <tngur1101@naver.com>
2024-02-06 12:55:12 +0900

    #50 - feat : Django 끝
    Django 끝
    
    Change-Id: I0482cf0703f83b08971df59dc25d85a57a144998

--- django/ByeolDam/ByeolDamAI/views.py
+++ django/ByeolDam/ByeolDamAI/views.py
@@ -32,12 +32,14 @@ def getImage(request):
         if response.status_code == 200:
             # 이미지를 저장할 경로 설정 (예시: media 폴더 내에 저장)
             base_path = 'media/'
-            filename = os.path.join(base_path, 'downloaded_image.jpg')
+            fileurl = 'downloaded_image.jpg'
+            filename = os.path.join(base_path, fileurl)
 
             # 이미지 저장
             f = open(filename, 'wb')
             f.write(response.content)
-            return JsonResponse({'image_url': "downloaded_image.jpg"})
+            result = runAI(filename)
+            return JsonResponse({'result': result})
         else:
             return JsonResponse({'error': 'Failed to download image'}, status=500)
 
@@ -47,7 +49,7 @@ def getImage(request):
 request에는 이미지 이름이 들어온다.
 runAI함수는 해당 이미지를 read하고 segmentation을 돌려서 윤곽선 데이터를 return한다.
 """
-def runAI(request):
+def runAI(filename):
     TORCH_VERSION = ".".join(torch.__version__.split(".")[:2])
     CUDA_VERSION = torch.__version__.split("+")[-1]
     print("torch: ", TORCH_VERSION, ", cuda: ", CUDA_VERSION)
@@ -56,8 +58,7 @@ def runAI(request):
     setup_logger()
 
     start = time.time()
-    imageFileName = getImage(request)["image_url"]
-    image = cv2.imread(imageFileName)
+    image = cv2.imread(filename)
 
     if image is None:
         return "이미지를 불러오는데 실패하였습니다!"
@@ -67,25 +68,28 @@ def runAI(request):
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
     predictor = DefaultPredictor(cfg)
     outputs = predictor(image)
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 
     boxes = (outputs["instances"].pred_boxes.tensor.cpu().numpy())
 
     masks = (outputs["instances"].pred_masks.cpu().numpy().astype('uint8'))
+    
     contours = []
     for pred_mask in outputs["instances"].pred_masks:
         mask = pred_mask.cpu().numpy().astype('uint8')
-        contours.append(contour[0])
         contour, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours.append(contour[0])
     
+    contours = [np.squeeze(contour) for contour in contours]
+
     dataset = []
     counter_box = 0
     for i, box in enumerate(boxes):
         data = {}
-        data["image_width"] = outputs["instances"].image_size[1]
         data["image_height"] = outputs["instances"].image_size[0]
+        data["image_width"] = outputs["instances"].image_size[1]
         data["bounding_box"] = box.tolist()
         data["contours"] = contours[i].tolist()
         data["id"] = f"id_{counter_box}"
