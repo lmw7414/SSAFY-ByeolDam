5645de8923c6aff3c61650e3397ad16ec1e617ea
이은규 <eung9702@gmail.com>
2024-02-06 23:49:26 +0900

    #51 - feat : 팔로워/팔로잉 모달 구현
    팔로잉, 팔로워 유저 정보 api 정보 불러오기 구현
    
    Change-Id: Ib2ff44c04b5029af318e41e9b5fbbebd11c86f97

--- frontend/src/components/modal/Follow/FollowContainer.jsx
+++ frontend/src/components/modal/Follow/FollowContainer.jsx
@@ -2,47 +2,79 @@ import { useState, useEffect } from 'react';
 import UserBox from './UserBox';
 import axios from '/src/apis/axios';
 
-export default function FollowingFollower({ followSubject }) {
-  const [followings, setFollowings] = useState(null);
-  const [followers, setFollowers] = useState(null);
+export default function FollowingFollower({ followSubject, modalState }) {
+  const [array, setArray] = useState([]);
 
-  // const readFollowings = async () => {
-  //   const data = await axios.get('/me/followings');
-  //   return data;
-  // };
-
-  // const readFollowers = async () => {
-  //   const data = await axios.get('/me/followers');
-  //   return data;
-  // };
-
-  // useEffect(() => {
-  //   {
-  //     followSubject === 'follow'
-  //       ? readFollowings().then((result) => {
-  //           setFollowings(result);
-  //         })
-  //       : readFollowers().then((result) => {
-  //           setFollowers(result);
-  //         });
-  //   }
-  // }, []);
-
-  const dummyFollowings = {
-    imageUrl: '/src/assets/images/space.png',
-    email: 'eung0202@naver.com',
-    name: '리은규',
-    nickname: '스트링치즈먹고싶다',
-    memo: '카페지즈에서 야간 작업중',
-    disclosureType: 'VISIBLE',
-    birthday: '1956-02-28',
+  const readFollowings = async () => {
+    const data = await axios.get('/me/followings');
+    // console.log('followings', data.data.result);
+    return data.data.result;
   };
+
+  const readFollowers = async () => {
+    const data = await axios.get('/me/followers');
+    // console.log('followers', data.data.result);
+    return data.data.result;
+  };
+
+  useEffect(() => {
+    if (followSubject === 'following') {
+      readFollowings().then((result) => {
+        setArray(result);
+      });
+    } else if (followSubject === 'follower') {
+      readFollowers().then((result) => {
+        setArray(result);
+      });
+    }
+  }, [modalState]);
+
+  // console.log(followSubject);
+  // console.log(array);
+  const dummyArray = [
+    {
+      imageUrl: '/src/assets/images/space.png',
+      email: 'eung0202@naver.com',
+      name: '리은규',
+      nickname: '스트링치즈먹고싶다',
+      memo: '카페지즈에서 야간 작업중',
+      disclosureType: 'VISIBLE',
+      birthday: '1956-02-28',
+    },
+    {
+      imageUrl: '/src/assets/images/space.png',
+      email: 'chiron7777@naver.com',
+      name: '리현쳘',
+      nickname: '배가부르다말다',
+      memo: '고치소사마 카레 세접시 쌉가능',
+      disclosureType: 'VISIBLE',
+      birthday: '1956-02-28',
+    },
+    {
+      imageUrl: '/src/assets/images/space.png',
+      email: 'lsh243@naver.com',
+      name: '리수혁',
+      nickname: '메이플하고시팓',
+      memo: '헤헤헿',
+      disclosureType: 'VISIBLE',
+      birthday: '1999-05-12',
+    },
+  ];
 
   return (
     <div>
       <input type="text" />
       <div>
-        <UserBox />
+        {dummyArray.map((user) => (
+          <UserBox
+            followSubject={followSubject}
+            key={user.nickname}
+            profileImg={user.imageUrl}
+            nickname={user.nickname}
+            // numConstellation={}
+            // numStar={}
+          />
+        ))}
       </div>
     </div>
   );
