5b0437df2ee5955a47756deaddafb0bee2084775
이은규 <eung9702@gmail.com>
2024-02-09 18:07:56 +0900

    #33 - feat : 내 별자리 정보 불러오기
    별자리 리스트 모달에 내 별자리 정보 배열 불러오기
    배열 map 순회하여 ConstellationRow 컴포넌트에 별자리별 정보 전달 구현
    
    Change-Id: I73778e5a88bed14b98d5e5ebfa92cccdfc931798

--- frontend/src/components/modal/ConstellationModal/ConstellationModal.jsx
+++ frontend/src/components/modal/ConstellationModal/ConstellationModal.jsx
@@ -6,26 +6,50 @@ import axios from '../../../apis/axios.js';
 import parseJwt from '../../../utils/parseJwt.js';
 
 export default function ConstellationModal() {
+  const [consteallationInfos, setConstellationInfos] = useState([]);
+
   const readConstellations = async () => {
     const JWTtoken = sessionStorage.getItem('token');
-    // const userInfo = sessionStorage.getItem('userInfo');
-    // console.log(JWTtoken);
-    // console.log(userInfo);
     const parsed = parseJwt(JWTtoken);
-
-    console.log(parsed);
-    const profile = await axios.get(`/constellations/user/${parsed.email}`);
+    // console.log(parsed.email);
+    const array = await axios.get(`/constellations/user/${parsed.email}`);
+    setConstellationInfos(array);
   };
 
   useEffect(() => {
     readConstellations();
   }, []);
+
+  const array = [
+    {
+      id: 0,
+      title: '갱얼쥐',
+      tag: 'string',
+      disclosure: 'VISIBLE',
+      hits: 45,
+      description: '멍멍멍뭉이',
+      createdAt: '2024-02-09T08:17:22.569Z',
+      modifiedAt: '2024-02-09T08:17:22.569Z',
+      deletedAt: '2024-02-09T08:17:22.569Z',
+    },
+    {
+      id: 1,
+      title: '고먐미',
+      tag: 'string',
+      disclosure: 'VISIBLE',
+      hits: 241,
+      description: '냐옹냐옹냐옹이',
+      createdAt: '2024-02-09T08:17:22.569Z',
+      modifiedAt: '2024-02-09T08:17:22.569Z',
+      deletedAt: '2024-02-09T08:17:22.569Z',
+    },
+  ];
   return (
     <div className="constellation-modal-container">
       <div className="">
-        {/* map으로 별자리 목록 전부 출력 예정 */}
-        <ConstellationRow />
-        <ConstellationRow />
+        {array.map((constellation) => (
+          <ConstellationRow key={constellation.id} title={constellation.title} />
+        ))}
       </div>
       <div className="constellation_main_buttons_box">
         <img
