35643627711d1093bdc6f8aef9db3deec10d02f5
이은규 <eung9702@gmail.com>
2024-02-11 14:04:37 +0900

    #47 - feat : 검색바/검색페이지 이동 구현
    검색바 아이콘 활성화시 페이지 이동 구현
    검색창 입력값 실시간 바인딩 구현
    
    Change-Id: I9f97678791da78a3c5f8c53f3a404e668d2194a9

---
+++ frontend/src/pages/search/SearchConstellation.jsx
@@ -0,0 +1,7 @@
+export default function SearchConstellation() {
+  return (
+    <div>
+      <h1>별자리 검색 페이지</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/search/SearchStar.jsx
@@ -0,0 +1,7 @@
+export default function SearchStar() {
+  return (
+    <div>
+      <h1>별 검색 페이지</h1>
+    </div>
+  );
+}
