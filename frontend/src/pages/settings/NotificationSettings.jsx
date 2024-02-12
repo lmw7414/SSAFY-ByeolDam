8989c9f0227a4fe4dcd04e6a0d1a5adabd40d558
차현철 <ckguscjf03@gmail.com>
2024-02-12 15:39:50 +0900

    #70 - feat : 설정 탭 알림 설정 제작
    navBar 폭 조절,
    settings.scss 코드 추가,
    알림 설정 페이지 제작
    
    Change-Id: I956ae65eea526b9ef88fbbab3fe1238dab1cb34d

--- frontend/src/pages/settings/NotificationSettings.jsx
+++ frontend/src/pages/settings/NotificationSettings.jsx
@@ -1,10 +1,48 @@
 export default function NotificationSettings() {
   return (
-    <div className="account-settings-container">
-      <h1 className="nickname">알림</h1>
-      <div>
-        <h2>푸시 알림</h2>
-        <div></div>
+    <div className="settings-page-container">
+      <h1 className="nickname settings-title">알림</h1>
+      <div className="notification-settings-menu">
+        <h2 className="nickname">푸시 알림</h2>
+        <div className="norification-menu-box">
+          <div className="toggle-slide-box">
+            <h3 className="nickname">모두 일시 중단</h3>
+            <div className="toggle">
+              <input type="checkbox" id="toggle-slide" />
+              <label for="toggle-slide">on/off</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr className="settings-detail-divide" />
+
+      <div className="notification-settings-menu">
+        <h2 className="nickname">좋아요</h2>
+        <div className="norification-menu-box">
+          <div className="radio-box">
+            <input type="radio" />
+            <p className="nickname">해제</p>
+          </div>
+          <div className="radio-box">
+            <input type="radio" />
+            <p className="nickname">내가 팔로우 하는 사용자</p>
+          </div>
+          <div className="radio-box">
+            <input type="radio" />
+            <p className="nickname">모든 사람</p>
+          </div>
+        </div>
+      </div>
+      <hr className="settings-detail-divide" />
+
+      <div className="notification-settings-menu">
+        <h2 className="nickname">푸시 알림</h2>
+        <div className="norification-menu-box">
+          <div className="radio-box">
+            <input type="radio" />
+            <p className="nickname">해제</p>
+          </div>
+        </div>
       </div>
     </div>
   );
