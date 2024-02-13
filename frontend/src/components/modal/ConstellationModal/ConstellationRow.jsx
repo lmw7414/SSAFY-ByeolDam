610cb14896acd51ef9750a6fb5f27e593960c042
이은규 <eung9702@gmail.com>
2024-02-13 23:36:11 +0900

    #33 - feat : 별자리 리스트 모달 api
    별자리 리스트 별자리 데이터 api 불러오기 및 CSS
    
    Change-Id: I90648647aab6fbfad4b590e1681d609d812f68b4

--- frontend/src/components/modal/ConstellationModal/ConstellationRow.jsx
+++ frontend/src/components/modal/ConstellationModal/ConstellationRow.jsx
@@ -1,10 +1,99 @@
-export default function ConstellationRow({ title }) {
+import { useState, useEffect } from 'react';
+
+import axios from '../../../apis/axios';
+
+export default function ConstellationRow({ title, constellationId, constellationThumb }) {
+  const [starArray, setStarArray] = useState([]);
+
+  const readStars = async () => {
+    const data = await axios.get(`articles/constellation/${constellationId}`);
+    // console.log(data);
+    setStarArray(data.data.result);
+  };
+
+  useEffect(() => {
+    readStars();
+  }, []);
+  const dummyArray = [
+    {
+      id: 0,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/1.jpg' },
+    },
+    {
+      id: 1,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/2.jpg' },
+    },
+    {
+      id: 2,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/3.jpg' },
+    },
+    {
+      id: 3,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/4.jpg' },
+    },
+    {
+      id: 4,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/5.jpg' },
+    },
+    {
+      id: 5,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/6.jpg' },
+    },
+    {
+      id: 6,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/7.jpg' },
+    },
+    {
+      id: 7,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/8.jpg' },
+    },
+    {
+      id: 8,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/9.jpg' },
+    },
+    {
+      id: 9,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/10.jpg' },
+    },
+    {
+      id: 10,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/11.jpg' },
+    },
+    {
+      id: 11,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/12.jpg' },
+    },
+    {
+      id: 12,
+      title: '',
+      imageResponse: { imageName: '', imageUrl: '/public/images/search-test/13.jpg' },
+    },
+  ];
   return (
     <div className="constellation-row">
       <div className="constellation-thumbnail-container">
+        <img src={constellationThumb} alt="" className="constellation-thumbnail" />
         <h3>{title}</h3>
       </div>
-      <div className="star-list-container">사진들</div>
+      <div className="star-list-container">
+        {dummyArray.map((star) => (
+          <div key={star.id} className="star-image-container">
+            <img src={star.imageResponse.imageUrl} alt="" className="star-image" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
