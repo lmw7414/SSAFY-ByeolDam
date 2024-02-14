cff2e640b6584682555002c6c451f5f039b90888
이창헌 <changbill@naver.com>
2024-02-14 21:51:04 +0900

    #81 - feat : 별자리 검색 기능 수정
    검색 시 회원 EntityList 대신 adminNickname 불러오기
    
    Change-Id: I589d02d0de84837a86bb5916b9fe8a19ee26f700

--- backend/src/main/java/com/ssafy/star/search/dto/response/ConstellationSearchResponse.java
+++ backend/src/main/java/com/ssafy/star/search/dto/response/ConstellationSearchResponse.java
@@ -1,17 +1,15 @@
 package com.ssafy.star.search.dto.response;
 
-import com.ssafy.star.constellation.dto.ConstellationUser;
 import com.ssafy.star.contour.dto.ContourResponse;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record ConstellationSearchResponse(
         Long id,
         String name,
         ContourResponse contourResponse,
         long hits,
-        List<ConstellationUser> constellationUsers,
+        String adminNickname,
         LocalDateTime createdAt,
         LocalDateTime modifiedAt
 ) {
