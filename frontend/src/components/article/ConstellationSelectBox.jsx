b1f55c048ce7229db937b15c6a8a1361be50e425
이은규 <eung9702@gmail.com>
2024-02-14 22:50:30 +0900

    #33 - fix : 게시글 생성_별자리 썸네일 이미지 수정
    및 CSS
    
    Change-Id: I17a7174051efa61b9e7b85d8b5f6d5459771a6a5

--- frontend/src/components/article/ConstellationSelectBox.jsx
+++ frontend/src/components/article/ConstellationSelectBox.jsx
@@ -6,7 +6,9 @@ export default function ConstellationSelectBox({ thumbnail, name, id, article, s
       className={`constellation-select-box ${article.constellationId === id ? 'constellation-select-box-selected' : ''}`}
       onClick={setSelected}
     >
-      <img src={thumbnail} />
+      <div className="constellation-select-box-thumbnail-container">
+        <img src={thumbnail} className="constellation-select-box-thumbnail" />
+      </div>
       <div>{name}</div>
     </div>
   );
