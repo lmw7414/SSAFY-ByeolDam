0f3960230bc0f2233cef181e27413e47c420e517
이은규 <eung9702@gmail.com>
2024-02-15 02:53:19 +0900

    #33 - style : 별자리 리스트 모달 CSS
    스타일링
    
    Change-Id: I356abcc481db05fe70e07659c036c83529fafb9e

--- frontend/src/components/modal/ConstellationModal/ConstellationRow.jsx
+++ frontend/src/components/modal/ConstellationModal/ConstellationRow.jsx
@@ -9,7 +9,7 @@ export default function ConstellationRow({
   hoverArticles,
 }) {
   const [modalState, setModalState] = useModal();
-
+  console.log(hoverArticles);
   const openAritcle = async (articleId) => {
     const { result } = await getArticles(articleId);
 
@@ -33,6 +33,7 @@ export default function ConstellationRow({
     <div className="constellation-row">
       <div className="constellation-thumbnail-container">
         <img src={constellationThumb} alt="" className="constellation-thumbnail" />
+        <p className="constellation-title">{title}</p>
       </div>
       <div className="star-list-container">
         {hoverArticles &&
