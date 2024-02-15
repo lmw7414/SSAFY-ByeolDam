cff115d389ccc6b903325deb9fb1276605657227
이은규 <eung9702@gmail.com>
2024-02-15 17:37:30 +0900

    #33 - style : 별자리 생성 페이지 CSS
    stage 테두리에 전부 border-radius
    
    Change-Id: I45a6aecc9336538b7044c313065f9405b0593f96

--- frontend/src/components/editor/ContoursEditor.jsx
+++ frontend/src/components/editor/ContoursEditor.jsx
@@ -69,6 +69,7 @@ export default function ContoursEditor({
       onMouseLeave={() => {
         editor.current.container().style.cursor = 'auto';
       }}
+      className="border-radius"
     >
       <Layer>
         <KonvaImage
