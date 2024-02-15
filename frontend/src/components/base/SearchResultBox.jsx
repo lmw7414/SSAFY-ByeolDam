38594aa8112edd0fcbeb3f88b5787f016e3afb78
차현철 <ckguscjf03@gmail.com>
2024-02-15 15:17:47 +0900

    #47 - bug : 게시물 검색 및 별자리 검색 기능 및 페이지 제작
    백엔드와의 통신 중 에러 발생
    
    Change-Id: I4cd3caf17fc2fcc35626c22dda03d05f9e319317

--- frontend/src/components/base/SearchResultBox.jsx
+++ frontend/src/components/base/SearchResultBox.jsx
@@ -1,24 +1,75 @@
-export default function SearchResultBox({ data }) {
+import { useState } from 'react';
+import { useEffect } from 'react';
+import axios from '../../apis/axios';
+import { getComments } from '../../apis/comments';
+
+export default function SearchResultBox({ data, filterId }) {
+  const [cntLike, setCntLike] = useState(0);
+  const [cntComment, setCntComment] = useState(0);
+
+  useEffect(() => {
+    // 게시물 검색
+    if (filterId === 1) {
+      const fetchData = async () => {
+        const { data: commentsResult } = await getComments(data.id);
+        setCntComment(commentsResult.length);
+      };
+
+      const getLikeCount = async (id) => {
+        const { data: likeData } = await axios.get(`/articles/${id}/likeCount`);
+        setCntLike(likeData.result);
+      };
+
+      getLikeCount(data.id);
+      fetchData();
+    } else if (filterId === 2) {
+      const getLikeCount = async (id) => {
+        const { data: likeData } = await axios.get(`/constellations/${id}/likeCount`);
+        setCntLike(likeData.result);
+      };
+
+      getLikeCount(data.id);
+    }
+  }, []);
+
   return (
     <div key={data.id} className="search-result">
       <div className="search-result-overlay">
         <div className="search-result-header">
-          <p>닉네임</p>
+          <p>{data.ownerEntityNickname || data.adminNickname}</p>
         </div>
         <div className="search-result-main">
-          <p>{data.title}</p>
+          <p id="title">{data.title}</p>
           <div className="search-result-main-content">
             <div className="search-result-main-description">
-              <p>{data.description}</p>
+              <p id="hashtag">{!!data.articleHashtags && data.articleHashtags.join(' ')}</p>
+              <p>{!!data.name && data.name}</p>
             </div>
             <div className="search-result-main-like-comment">
-              <p>좋아요</p>
-              <p>댓글</p>
+              <div className="search-like-comment">
+                <img src="/images/search-result/feed_like.png" alt="좋아요 이미지" />
+                <p>{cntLike}</p>
+              </div>
+              <div className="search-like-comment">
+                {filterId === 1 ? (
+                  <img src="/images/search-result/feed_comment.png" alt="댓글 이미지" />
+                ) : (
+                  <img src="/images/search-result/feed_star_icon.png" alt="별 이미지" />
+                )}
+                <p>{filterId === 1 ? cntComment : data.articleCnt}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <img src={data.imgUrl} alt="" className="search-result-image" />
+      <img
+        src={
+          (!!data.imageResponse && data.imageResponse.imageUrl) ||
+          (!!data.contourResponse && data.contourResponse.cThumbUrl)
+        }
+        alt=""
+        className="search-result-image"
+      />
     </div>
   );
 }
--- frontend/src/pages/search/SearchStar.jsx
+++ frontend/src/pages/search/SearchStar.jsx
@@ -1,90 +1,62 @@
 import SearchResultBox from '../../components/base/SearchResultBox';
+import { useEffect, useState } from 'react';
+import useModal from '../../hooks/useModal';
+import { useLocation } from 'react-router-dom';
+import axios from '../../apis/axios';
+import ArticleModal from '../../components/modal/article/ArticleModal';
 
 export default function SearchStar() {
-  const dummyPosts = [
-    {
-      id: 1,
-      title: 1,
-      description: '1입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/1.jpg',
-    },
-    {
-      id: 2,
-      title: 2,
-      description: '2입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/2.jpg',
-    },
-    {
-      id: 3,
-      title: 3,
-      description: '3입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/3.jpg',
-    },
-    {
-      id: 4,
-      title: 4,
-      description: '4입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/4.jpg',
-    },
-    {
-      id: 5,
-      title: 5,
-      description: '5입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/5.jpg',
-    },
-    {
-      id: 6,
-      title: 6,
-      description: '6입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/6.jpg',
-    },
-    {
-      id: 7,
-      title: 7,
-      description: '7입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/7.jpg',
-    },
-    {
-      id: 8,
-      title: 8,
-      description: '8입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/8.jpg',
-    },
-    {
-      id: 9,
-      title: 9,
-      description: '9입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/9.jpg',
-    },
-    {
-      id: 10,
-      title: 10,
-      description: '10입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/10.jpg',
-    },
-    {
-      id: 11,
-      title: 11,
-      description: '11입니당',
-      articleHashtags: ['태그', '가족'],
-      imgUrl: '/images/search-test/11.jpg',
-    },
-  ];
+  const [articles, setArticles] = useState([]);
+  const [modalState, setModalState] = useModal();
+  const location = useLocation();
+
+  const keyword = location.state === null ? '' : location.state.keyword;
+  const filterId = location.state === null ? 0 : location.state.filterId;
+
+  useEffect(() => {
+    searchArticle(keyword);
+  }, [keyword]);
+
+  const searchArticle = async (keyword) => {
+    const { data } = await axios.get(`/search/title?keyword=${keyword}`);
+    console.log(data);
+    setArticles(data.result);
+  };
+
+  const openArticleModal = (data) => {
+    console.log(data);
+    setModalState({
+      isOpen: true,
+      title: '게시물 상세',
+      children: (
+        <ArticleModal
+          articleId={data.id}
+          description={data.title}
+          hits={data.hits}
+          createdAt={data.createdAt}
+          imgUrl={data.imageResponse.imageUrl}
+          owner={data.ownerEntityNickname}
+          tags={data.articleHashtags}
+          commentList={data.commentList}
+          constellationName={data.constellationEntityName}
+          constellationId={data.constellationId}
+        />
+      ),
+    });
+  };
+
   return (
     <div className="search-page-background">
       <div className="search-result-container">
-        {dummyPosts.map((post) => (
-          <SearchResultBox key={post.id} data={post} />
+        {articles.map((data) => (
+          <SearchResultBox
+            key={data.id}
+            data={data}
+            filterId={filterId}
+            onClick={() => {
+              openArticleModal(data);
+            }}
+          />
         ))}
       </div>
     </div>
