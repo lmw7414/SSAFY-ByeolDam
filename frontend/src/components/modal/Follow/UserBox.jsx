8ddf3d7d1f4c15d017834bf06206bea92a7ffbf3
이은규 <eung9702@gmail.com>
2024-02-15 14:04:46 +0900

    #51 - feat : 팔로우모달 구현
    팔로잉 / 팔로워 api 불러오기
    CSS 스타일링
    
    Change-Id: I04494b0404618124caac98a41a80ee409b1bc59c

--- frontend/src/components/modal/Follow/UserBox.jsx
+++ frontend/src/components/modal/Follow/UserBox.jsx
@@ -1,16 +1,38 @@
-export default function UserBox({ followSubject, profileImg, nickname }) {
+export default function UserBox({
+  followSubject,
+  profileImg,
+  nickname,
+  constellationCounts,
+  articleCounts,
+}) {
   return (
-    <div>
-      <div className="follow-modal-profile-img-container">
-        <img className="follow-modal-profile-image" src={profileImg} alt="" />
+    <div className="user-box">
+      <div className="user-box-left">
+        <div className="follow-modal-profile-img-container">
+          <img className="follow-modal-profile-image" src={profileImg} alt="" />
+        </div>
+        <p className="real-nickname">{nickname}</p>
       </div>
-      <p>{nickname}</p>
-      <div>
-        <img src="" alt="" />
-
-        <img src="" alt="" />
-
-        <button>{followSubject === 'following' ? '팔로잉' : '삭제'}</button>
+      <div className="user-box-right">
+        <div className="icon-num-box">
+          <img
+            src="/images/search-bar-filter-icons/constellation_activated.png"
+            alt="constellation-icon"
+            className="user-box-icon"
+          />
+          <p className="user-box-num">{constellationCounts}</p>
+        </div>
+        <div className="icon-num-box">
+          <img
+            src="/images/search-bar-filter-icons/star_activated.png"
+            alt="star-icon"
+            className="user-box-icon"
+          />
+          <p className="user-box-num">{articleCounts}</p>
+        </div>
+        <button className="follow-modal-button">
+          {followSubject === 'following' ? '팔로잉' : '삭제'}
+        </button>
       </div>
     </div>
   );
