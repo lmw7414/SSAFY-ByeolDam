2d77e420edd830c39beda4ff8bbb68c2f0c9fd72
차현철 <ckguscjf03@gmail.com>
2024-02-15 15:08:57 +0900

    #47 - feat : 유저 검색 탭 및 기능 제작
    유저의 닉네임을 기준으로 검색 시 관련 유저 리스트 반환
    유저의 프로필 이미지가 없다면 기본 이미지로 표시
    
    Change-Id: I8ea8324fdb53b68e0c81267bd4c8a258470ea4f6

---
+++ frontend/src/components/search/UserView.jsx
@@ -0,0 +1,14 @@
+export default function UserView({ data }) {
+  const ImgUrl = data.imageUrl != '' ? data.imageUrl : 'default_profile_image.png';
+
+  return (
+    <div className="user-view-container">
+      <div className="user-view">
+        <div className="user-view-img-container">
+          <img className="user-view-img" src={ImgUrl} alt="사용자의 프로필 사진" />
+        </div>
+        <p className="user-view-name">{data.nickname}</p>
+      </div>
+    </div>
+  );
+}
